b0e3792ab80cc6e7717c1f1311bd375f11727bc9
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-08-07 09:23:31 -0300

    Añade html email mejorado

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
@@ -8,6 +7,9 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+
+
 
 let whiteList = [process.env.URL_PORTFOLIO]
 
@@ -24,18 +26,132 @@ let corsOptions = {
 app.get('/', (req, res) => {
     res.send("Server para envios de email");
 });
+app.get('/email', (req, res) =>{ 
+    res.render('email',{
+        title:'Formato de email'
+    });
+});
+const estructuraHTML = (content)=> {
+  let contenido =
+    `<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulario de contacto</title>
+    </head>
+    
+    <body style="margin: 0;padding: 0;outline: 0;box-sizing: border-box;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;-webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale; background-color: #F7F8FA;">
+        <main>
+        <header style="background-color: #0a71c6d1; width: auto;" >
+        <div>
+          <div style="flex-shrink: 0;
+          width: 100%;
+          max-width: 100%;
+          padding-right: calc(var(--bs-gutter-x) * .5);
+          padding-left: calc(var(--bs-gutter-x) * .5);
+          margin-top: var(--bs-gutter-y);">
+            <nav style="width: 100%;
+            padding-right: var(--bs-gutter-x,.75rem);
+            padding-left: var(--bs-gutter-x,.75rem);
+            margin-right: auto;
+            margin-left: auto;">
+              <ul style="list-style: none;
+              display: inline;">
+                <li style="color: #fff;
+                font-size: 35px;
+                margin: 10px;
+                display: inline;
+            ">
+                  <a style="text-decoration: none; color: #C1DBF1;padding: 5px;"
+                    href="https://www.linkedin.com/in/mendietasergio/"
+                    target="_blank"
+                    title="LinkedIn"
+                  >
+                    <img style=" width: 50px; height: 50px" src="img/LinkedIn.png" alt="">
+                  </a>
+                </li>
+                <li style="color: #fff;
+                font-size: 35px;
+                margin: 10px;
+                display: inline;
+            ">
+                  <a style="text-decoration: none; color: #C1DBF1;padding: 5px;"
+                    href="https://github.com/MendietaSergio"
+                    target="_blank"
+                    title="GitHub"
+                  >
+                    <!-- <i class="fab fa-github-alt"></i> -->
+                    <img style=" width: 50px; height: 50px" src="/img/GitHub.png" alt="">
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+        </main>
+        <article>
+          <div style="width: 100%; padding-right: var(--bs-gutter-x,.75rem); padding-left: var(--bs-gutter-x,.75rem); margin-right: auto; margin-left: auto;">
+            <div style="flex-shrink: 0;
+            width: 100%;
+            max-width: 100%;
+            padding-right: calc(var(--bs-gutter-x) * .5);
+            padding-left: calc(var(--bs-gutter-x) * .5);
+            margin-top: var(--bs-gutter-y);">
+            <div style="margin-top: 3rem!important; justify-content: center; display: grid; color: black">
+            <h1 style="margin-bottom: 1.5rem!important;">Hola ${content.nombre} !</h1>
+            <h4 style="margin-bottom: 1.5rem!important;">Gracias por visitar mi <a style="text-decoration: none; color: #0088ff;padding: 5px;" class="url_portfolio" target="_blank" title="Portafolio" href="https://smendietaportfolio.herokuapp.com/">Portafolio</a> y contactarme :D</h4>
+            <span>Este es el formulario que me has enviado:</span>
+            <br/>
+            <ul style="list-style: none;
+            display: inline;">
+                <li>Nombre: ${content.nombre}</li>
+                <li>Email: ${content.email}</li>
+                <li>Asunto: ${content.asunto}</li>
+            </ul>
+            <h4 style="margin: 1.rem!important;">Mensaje</h4>
+            <span>${content.comentario}</span>
+          </div>
+            </div>
+          </div>
+        </article>
+    
+        <footer style="background-color:#41A4F5; width: 100%; height: 40px; position: absolute; bottom: 0;">
+          <div style="width: 100%;
+          padding-right: var(--bs-gutter-x,.75rem);
+          padding-left: var(--bs-gutter-x,.75rem);
+          margin-right: auto;
+          margin-left: auto;">
+              <div style="flex-shrink: 0;
+              width: 100%;
+              max-width: 100%;
+              padding-right: calc(var(--bs-gutter-x) * .5);
+              padding-left: calc(var(--bs-gutter-x) * .5);
+              margin-top: var(--bs-gutter-y);">
+                  <div >
+                      <div>
+                          <h5 style="text-align: center; font-size: .9rem; padding-top: 10px; color: black;">Plantilla Copyright © Portafolio <a 
+                              href="http://linkedin.com/in/mendietasergio" target="_blank" title="Portafolio" style="text-decoration: none; color: black;padding: 5px;"
+                              >Mendieta Sergio</a></h5>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </footer>
+      </body>
+      </html>
+    `;
+    return contenido
+  }
+app.post("/api/form", (req, res) => {
+    const content = req.body;
+    const contentHTML = estructuraHTML(content)
 
-app.post("/api/form",cors(corsOptions), (req, res) => {
-    const contentHTML = `
-        <h3>Email enviado desde React</h3>
-        <ul>
-            <li>Nombre: ${req.body.nombre}</li>
-            <li>Email: ${req.body.email}</li>
-            <li>Asunto: ${req.body.asunto}</li>
-        </ul>
-        <h3>Mensaje</h3>
-        <p>${req.body.comentario}</p>`
-    console.log(contentHTML);
+    console.log(estructuraHTML);
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         secureConnection: true,
