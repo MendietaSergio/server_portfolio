43d2f388b32390e2657d3ad7b53951d4ad77263c
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-14 14:04:48 -0300

    Añade ruta '/api/proyect' para mostrar lista de proyectos

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
+var proyectsRouter = require('./routes/proyects');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(cors());
 
 
 app.use('/', indexRouter);
+app.use('/api', proyectsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/proyects.js
+++ routes/proyects.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const {listProyects} = require('../controllers/apiController')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/proyects', listProyects);
 
 module.exports = router;
