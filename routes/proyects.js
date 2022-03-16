470b3a049931504dc5c6b84dc55dabf2a235a242
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-16 17:21:39 -0300

    Añade cambio a ruta users por proyects, agrega get y post para mostrar y cargar nuevo proyecto

--- routes/proyects.js
+++ routes/proyects.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const {fileUpload} = require('../controllers/apiController')
 
-const {listProyects} = require('../controllers/apiController')
+const {listProyects,newProyects} = require('../controllers/apiController')
 /* GET users listing. */
 router.get('/proyects', listProyects);
+router.post('/proyects', newProyects);
 
 module.exports = router;
