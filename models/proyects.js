d78ee2bcca199460d616e1289005a0aeb4ec522c
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-16 17:20:11 -0300

    Añade model proyects

---
+++ models/proyects.js
@@ -0,0 +1,26 @@
+const {Schema, model} = require('mongoose')
+
+const ProyectsSchema = Schema(
+    {
+        title:{
+            type: String,
+            require:[true, 'El nombre del proyecto es obligatorio']
+        },
+        githubUrl:{
+            type:String,
+            require: [true, "La URL del GitHub, es obligatorio"]
+        },
+        deployUrl:{
+            type: String,
+            require: [true, "La URL del deploy, es obligatorio"]
+        },
+        titleImg:{
+            type: String,
+            require: [true, "La URL de la imagen, es obligatorio"]
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+module.exports = model('Proyects', ProyectsSchema)
